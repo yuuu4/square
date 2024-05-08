f2c8e81ce7f3fba6803cda6f39ab0acc10559996
yuuu4 <yurika@gmail.com>
2024-05-08 18:47:06 +0900

    delete

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -40,4 +40,9 @@ class PostController extends Controller
 
     return redirect('/posts/' . $post->id);
 }
+  public function delete(Post $post)
+  {
+      $post->delete();
+      return redirect('/');
+  }
 }
--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,7 @@ Route::middleware('auth')->group(function () {
     Route::post('/posts',[PostController::class,'store']);
     Route::get('/posts/{post}/edit',[PostController::class,'edit']);
     Route::put('/posts/{post}',[PostController::class,'update']);
+    Route::delete('/posts/{post}',[PostController::class,'delete']);
 });
 
 
