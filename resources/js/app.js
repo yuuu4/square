0103c0fa94a28b4b558b25c7ebe45336ecc1b5a9
yuuu4 <yurika@gmail.com>
2024-06-23 18:13:01 +0900

    update package.json

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,8 @@
 import './bootstrap';
 import Alpine from 'alpinejs';
-import $ from 'jquery';
-window.$ = $;
-window.jQuery = $;
+// import $ from 'jquery';
+// window.$ = $;
+// window.jQuery = $;
 
 
 window.Alpine = Alpine;
